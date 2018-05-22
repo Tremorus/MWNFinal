8cd1cc648353338e06b96a3aec08b3b80e0823dd
Андрей Милованов <tremoridze@gmail.com>
2018-05-22 17:12:32 +0300

    Added some changes for successful Azure publishing

--- MWN/Startup.cs
+++ MWN/Startup.cs
@@ -35,7 +35,7 @@ namespace MWN
             services.AddMvc();
 
             services.AddDbContext<MWNContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("MWNContext")));
+                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
